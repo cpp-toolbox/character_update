028798c3c7c50f2b5cf81439db85657802cf162a
cuppajoeman <ccn@cuppajoeman.com>
2024-07-29 00:52:15 -0400

    formatting

--- character_update.hpp
+++ character_update.hpp
@@ -3,8 +3,9 @@
 
 #include "sbpt_generated_includes.hpp"
 
-void update_player_camera_and_velocity(JPH::Ref<JPH::CharacterVirtual> &character, Camera &camera, Mouse &mouse,
-                                       NetworkedInputSnapshot &input_snapshot, float movement_acceleration,
-                                       double time_since_last_update, JPH::Vec3 gravity);
+std::function<void(double)> update_player_camera_and_velocity_closure(JPH::Ref<JPH::CharacterVirtual> &character,
+                                                                      Camera &camera, Mouse &mouse,
+                                                                      NetworkedInputSnapshot &input_snapshot,
+                                                                      float &movement_acceleration, JPH::Vec3 &gravity);
 
-#endif //CHARACTER_UPDATE_HPP
+#endif // CHARACTER_UPDATE_HPP
